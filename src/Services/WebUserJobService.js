fe6b33d5c6f8d00a6aeba0305a88bc8cf9051b6d
sizheng <sizheng89@gmail.com>
2021-08-24 16:02:51 +0800

    changes to test

--- src/Services/WebUserJobService.js
+++ src/Services/WebUserJobService.js
@@ -26,7 +26,7 @@ class DataService {
   getAllJobs() {
     // this is the changes I made. Might need to change back 
     // return axios.get("http://localhost:8080/api/webuser/job/list");
-    return axios.get("http://54.174.215.223:8080/webapp/api/webuser/job/list");
+    return axios.get("http://54.174.215.223:8080/webapp/api/job/list");
   }
   
   getUserUseRefreshToken(){
